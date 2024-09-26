208a7610100e6f2830e3c37bec140f03795b028f
Lea Rosema <lea@lea.lgbt>
2024-09-27 00:46:48 +0200

    chore: initial commit

---
+++ LeaEquipmentLister.lua
@@ -0,0 +1,76 @@
+LeaEquipmentLister = LibStub("AceAddon-3.0"):NewAddon("LeaEquipmentLister", "AceConsole-3.0")
+
+local AceGUI = LibStub("AceGUI-3.0")
+local isOpen = false
+
+function LeaEquipmentLister:OnInitialize()
+  LeaEquipmentLister:Print("type /eqlist for Lea's Equipment Lister")
+  LeaEquipmentLister:RegisterChatCommand("eqlist", "ShowDialog")
+end
+
+function GetItemsMarkup()
+  local str = [[
+<!DOCTYPE html>
+<html>
+<head>
+<title>Moo</title>
+<style>html{color-scheme: dark-light; font-family: sans-serif;}</style>
+</head>
+<body>
+]]
+  str = str .. "<h1>Inventory of " .. UnitFullName("player") .. "</h1>\n"
+  for bag = 0,4 do
+    str = str .. "<h2>Bag " .. bag .. "</h2>\n"
+    local slots = C_Container.GetContainerNumSlots(bag)
+    for slot = 1,slots do
+      local item = C_Container.GetContainerItemInfo(bag, slot)
+
+      if item == nil then
+        str = str .. "free slot\n"
+      else
+        str = str .. "<a data-wowhead=\"item=" .. item.itemID .. "\" href=\"https://www.wowhead.com/classic/item=" .. item.itemID .. "\" data-quality=\"" .. item.quality .. "\">" .. item.itemName .. "</a><br>"
+      end
+    end
+    str = str .. "\n\n"
+  end
+  
+  str = str .. [[
+  <script>const whTooltips = {colorLinks: true, iconizeLinks: true, renameLinks: true};</script>
+  <script src="https://wow.zamimg.com/js/tooltips.js"></script>
+</body>
+</html>
+]]
+  return str
+end
+
+
+function LeaEquipmentLister:ShowDialog(input)
+  if isOpen then
+    return
+  end
+  isOpen = true
+
+  local frame = AceGUI:Create("Frame")
+  frame:SetTitle("Example Frame")
+  frame:SetStatusText("AceGUI-3.0 Example Container Frame")
+  frame:SetCallback("OnClose", function(widget) isOpen = false;AceGUI:Release(widget) end)
+  frame:SetLayout("Flow")
+  
+  local editbox = AceGUI:Create("MultiLineEditBox")
+  editbox:SetRelativeWidth(1)
+  editbox:SetNumLines(20)
+  editbox:SetText(GetItemsMarkup())
+  frame:AddChild(editbox)
+  
+  local button = AceGUI:Create("Button")
+  button:SetText("Refresh")
+  button:SetWidth(200)
+  frame:AddChild(button)
+  local muh = C_Container.GetContainerItemInfo(0, 1)
+  print (muh.itemName)
+  button:SetCallback("OnClick", function(widget) editbox:SetText(GetItemsMarkup()) end)
+  
+end
+
+
+-- print(C_Container.GetContainerItemInfo(1, 1).itemName)
